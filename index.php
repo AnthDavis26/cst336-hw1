08ffd683fd9f58d46148edc16b7ac9f25e5bc1ee
AnthDavis26 <42490176+AnthDavis26@users.noreply.github.com>
2019-07-01 16:11:26 -0700

    test commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+		<meta charset="utf-8" />
+		<title> HW1 </title>
+		<link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+		<header>
+			<h1><b>ANTHONY DAVIS</b></h1>
+		</header>
+		
+		<!-- Thematic change in content -->
+		<hr /> 
+        <nav>
+			<b><a href="index.php">nav1</a></b>
+			<a href="about.php">nav2</a>
+			<a href="contact.php">nav3</a>
+		</nav>
+		
+		<br /><br />
+		
+		<main>
+			
+			<div>
+				Main<br />
+			</div>
+			
+		</main>
+		
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+			<hr>
+				Footer<br />
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
