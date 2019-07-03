396e174fa4452649f0cb714bb848e932dcb9665b
AnthDavis26 <42490176+AnthDavis26@users.noreply.github.com>
2019-07-02 21:25:10 -0700

    Not doing very well. Gotta get it done.

--- autopush.bat
+++ autopush.bat
@@ -1,3 +1,3 @@
 git add .
-git commit -m "I'm testing my batch file"
+git commit -m "Not doing very well. Gotta get it done."
 git push
--- languages.php
+++ languages.php
@@ -33,7 +33,47 @@ July 2, 2019
 			
 			<!-- Main content -->
 			<main>
-				<p>Test</p>
+			
+				<article>
+					<div id="article_title">C/C++ LANGUAGE</div>
+					<p>Generally speaking, modern-day games are programmed on the back of C++, basically the  <br/>
+					   evolution of the C language. With the extensive support of software that utilize the speed<br/> 
+					   of C++, it is the backbone of major game engines such as 
+					   <a href="https://docs.unrealengine.com/en-US/Programming/Introduction/index.html">Unreal Engine.</a></p>
+				</article>
+				<br />
+				<hr>
+				
+				<article>
+					<div id="article_title">ASSEMBLY</div>
+					<p>Prior to C++ becoming standard for game programming, assembly language was used due to <br/>
+					   limited hardware capabilities on home consoles. While assembly languages (which vary   <br/>
+					   depending on the processor used) are a step closer to machine language, they can grow<br/>
+					   complicated for large projects. Hardware is faster and more capable now, leaving assembly<br/> 
+					   language more useful for less-capable hardware or occasional inline programming in C.<br/><br/>
+					   Games like <a href="http://www.chrissawyergames.com/faq3.htm">RollerCoaster Tycoon</a> are a 
+					   testament to just how optimized assembly scripts<br/>
+					   can be.</p>
+				</article>
+				<br />
+				<hr>
+			
+				<article>
+					<div id="article_title">JAVA</div>
+					<p>Unlike C/C++ and assembly language, Java focuses on clear programming and compatibility <br/>
+					   with as many hardware devices and operating systems as possible. Due to the implementation<br/>
+					   of Java's Virtual Machine, along with behind-the-scenes management of unneeded memory, it <br/>
+					   does not necessarily work fast enough for large-scale games. Possibly the most well-known <br/>
+					   game to be developed with Java is <a href="https://www.minecraft.net/en-us/">Minecraft</a> 
+					    (although it has since been branched and ported<br/>
+					   to C++ for 
+					   <a href="https://www.microsoft.com/en-us/p/minecraft-for-windows-10/9nblggh2jhxj?activetab=pivot%3Aoverviewtab">Microsoft platforms</a> 
+					   to increase performance while maintaining cross-platform online<br/>
+					   multiplayer).</p>
+				</article>
+				<br />
+				<hr>
+				
 			</main>
 		</section>
 		
